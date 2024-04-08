6f12f0331d0682ded998d910ea1afc822dd9f843
Lars Kristiansen <larkr23@student.sdu.dk>
2024-04-08 19:00:24 +0200

    First commit

---
+++ .idea/src/java/DamLogo.java
@@ -0,0 +1,15 @@
+public class DamLogo {
+    String logoId;
+
+    public DamLogo(String logoId) {
+        this.logoId = logoId;
+    }
+
+    public String getLogoId() {
+        return logoId;
+    }
+
+    public void setLogoId(String logoId) {
+        this.logoId = logoId;
+    }
+}
